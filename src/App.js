63a96f6811c3c6cf9c9ec6136be924f0369bf17f
Erik Shelton <erikshelton@Eriks-Air.lan1>
2020-10-21 11:17:19 -0400

    complete base App.js, file structure cleaning

--- src/App.js
+++ src/App.js
@@ -1,26 +1,131 @@
-import React from 'react';
-import logo from './logo.svg';
+//import React
+import React, { useEffect } from 'react';
+//import App stylesheet
 import './App.css';
+//Import React Router Items
+import { Route, Link, Switch } from 'react-router-dom';
+//Import Display Component
+import Display from './Display';
+//Import Form Component
+import Form from './Form';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	//URL VARIABLE
+	const url = 'https://rc-app-backend.herokuapp.com';
+	//STATE TO HOLD RC VEHICLES
+	const [rc_Vehicles, setRC_Vehicles] = useState([]);
+	//STATE TO HOLD RC COMPANIES
+	const [rc_Companies, setRC_Companies] = useState([]);
+	//STATE TO HOLD SELECTED RC VEHICLE
+	const [selectedVehicle, setSelectedVehicle] = useState(emptyVehicle);
+
+	//empty rc vehicle for Form
+	const emptyVehicle = {
+		name: '',
+		img: '',
+	};
+
+	//FUNCTION TO FETCH COMPANIES
+	const getCompanies = () => {
+		fetch(url + '/rc_company')
+			.then((response) => response.json)
+			.then((data) => {
+				setRC_Companies(data);
+			});
+	};
+
+	//FUNCTION TO FETCH VEHICLES
+	const getVehicles = () => {
+		fetch(url + '/rc_vehicle')
+			.then((response) => response.json)
+			.then((data) => {
+				setRC_Vehicles(data);
+			});
+	};
+
+	//Get vehicles on page load
+	useEffect(() => {
+		getVehicles();
+		getCompanies();
+	}, []);
+
+	//handleCreate Function for creating vehicles
+	const handleCreate = (newVehicle) => {
+		fetch(url + '/rc_vehicle/', {
+			method: 'post',
+			headers: {
+				'Content-Type': 'application/json',
+			},
+			body: JSON.stringify(newVehicle),
+		}).then((response) => getVehicles());
+	};
+
+	//handleUpdate to update a vehicle when form is clicked
+	const handleUpdate = (rcVehicle) => {
+		fetch(url + '/rc_vehicle/' + rcVehicle._id, {
+			method: 'put',
+			headers: {
+				'Content-Type': 'application/json',
+			},
+			body: JSON.stringify(rcVehicle),
+		}).then((response) => getVehicles());
+	};
+
+	//selectVehicle which selects a vehicle
+	const selectVehicle = (vehicle) => {
+		setSelectedVehicle(vehicle);
+	};
+
+	//deleteVehicle function to delete a vehicle
+	const deleteVehicle = (vehicle) => {
+		fetch(url + '/rc_vehicle/' + vehicle._id, {
+			method: 'delete',
+			headers: {
+				'Content-Type': 'application/json',
+			},
+		}).then(() => {
+			getVehicles();
+		});
+	};
+
+	return (
+		<div className='App'>
+			<h1>RC VEHICLES AND COMPANIES</h1>
+			<hr />
+			<Link to='/create'>
+				<button>Add Vehicle</button>
+			</Link>
+			<main>
+				<Switch>
+					<Route
+						exact
+						path='/'
+						render={(rp) => (
+							<Display
+								{...rp}
+								companies={rc_Companies}
+								vehicles={rc_Vehicles}
+								selectVehicle={selectVehicle}
+								deleteVehicle={deleteVehicle}
+							/>
+						)}
+					/>
+					<Route
+						exact
+						path='/create'
+						render={(rp) => (
+							<Form
+								{...rp}
+								label='create'
+								vehicle={emptyVehicle}
+								handleSubmit={handleCreate}
+							/>
+						)}
+					/>
+				</Switch>
+			</main>
+		</div>
+	);
 }
 
 export default App;
