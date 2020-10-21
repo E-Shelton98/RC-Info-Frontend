ac427a5fff59cd98a0651584ba916578e863a0fc
Erik Shelton <erikshelton@Eriks-Air.lan1>
2020-10-21 11:24:15 -0400

    complete src/Display base

---
+++ src/Display.js
@@ -0,0 +1,30 @@
+import React from 'react'
+
+const Display = (props) => {
+
+    const loaded = () => (
+        <div style={{ textAlign: 'center' }}>
+            {props.rc_vehicles.map((vehicle) => (
+                <article>
+                    <img src={vehicle.img} alt=""/>
+                    <h3>{vehicle.name}</h3>
+                    <button onClick={() => {
+                        props.selectVehicle(vehicle)
+                        props.history.push('/edit')
+                    }}>
+                        Edit
+                    </button>
+                    <button onClick={() => {
+                        props.deleteVehicle(vehicle)
+                    }}>
+                        Delete
+                    </button>
+                </article>
+            ))}
+        </div>
+    )
+
+    return props.vehicle.length > 0 ? loaded() : <h1>Loading...</h1>
+}
+
+export default Display
